3228174e446e845ba58b05f94bcbe01107929cca
Anwesh43 <anweshthecool0@gmail.com>
2019-09-03 23:17:20 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcolorfillcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcolorfillcircleview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedcolorfillcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.colorfillcircleview.ColorFillCircleView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ColorFillCircleView.create(this)
+        fullScreen()
+    }
+
+    fun MainActivity.fullScreen() {
+        supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
