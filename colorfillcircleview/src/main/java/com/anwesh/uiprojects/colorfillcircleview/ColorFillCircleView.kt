b6afa5c419bacc8fcb9961919e8ce173db88d729
Anwesh43 <anweshthecool0@gmail.com>
2019-09-03 22:28:01 +0530

    adding ColorFillCircleView class

--- colorfillcircleview/src/main/java/com/anwesh/uiprojects/colorfillcircleview/ColorFillCircleView.kt
+++ colorfillcircleview/src/main/java/com/anwesh/uiprojects/colorfillcircleview/ColorFillCircleView.kt
@@ -68,3 +68,21 @@ fun Canvas.drawCFCNode(i : Int, scale : Float, sc : Float, paint : Paint) : Floa
     restore()
     return sc2 
 }
+
+class ColorFillCircleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
