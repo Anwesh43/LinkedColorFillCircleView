008d02f2fec144fa5fdd35cbd600ff085d03283d
Anwesh43 <anweshthecool0@gmail.com>
2019-09-03 10:35:14 +0530

    adding constants and libraries

--- colorfillcircleview/src/main/java/com/anwesh/uiprojects/colorfillcircleview/ColorFillCircleView.kt
+++ colorfillcircleview/src/main/java/com/anwesh/uiprojects/colorfillcircleview/ColorFillCircleView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.colorfillcircleview
 /**
  * Created by anweshmishra on 03/09/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+
+val colors : Array<String> = arrayOf("#880E4F", "#0D47A1", "#00C853", "#f44336", "#006064")
+val scGap : Float = 0.05f
+val rFactor : Float = 3.4f
+val backColor : Int = Color.parseColor("#BDBDBD")
